a02f5eb27196672d49d3a9329d382012846ba6b9
Leonardo Lima <leo.lima21051990@gmail.com>
2021-01-11 15:08:11 -0300

    add grayscale option

---
+++ scripts/gray_picamera_ros.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Bibliotecas necessarias
+import rospy
+import cv2
+import time
+import numpy as np
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+# Funcao q ira publicar os comandos no topico
+def talker():
+    # Objeto q ira publicar no topico teleop_topic
+    pub = rospy.Publisher('picamera/image_raw', Image, queue_size=10)
+    
+    # Inicio do node teleop_pub
+    rospy.init_node('picamera_node', anonymous=True)
+
+    # Criacao do ojeto de conversao
+    bridge = CvBridge()
+
+    # Setup da camera 
+    cap = cv2.VideoCapture(0)
+
+    # Setup de largura e altura da captura
+    cap.set(3, 320)
+    cap.set(4, 240)
+
+    while not rospy.is_shutdown():           
+        # Captura de um frame na camera
+        ret, cv2_frame = cap.read()
+        
+        # Converter para escala de cinza
+        gray_cv2_frame = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2GRAY)
+
+        # Converter a img cv2 em img ROS
+        ros_frame = bridge.cv2_to_imgmsg(gray_cv2_frame, "bgr8")
+    
+        # Publicacao da img no topico
+        rospy.loginfo(ros_frame)    
+        pub.publish(ros_frame)
+
+# Funcao main
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException: pass
